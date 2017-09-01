87f34e554967657f29cf681dcd4b13eced8c76a7
Thibault Jamet <tjamet@users.noreply.github.com>
2017-09-01 12:05:35 +0200

    Add photo page
    Signed-off-by: Thibault Jamet <tjamet@users.noreply.github.com>

---
+++ src/screens/photos/index.js
@@ -0,0 +1,34 @@
+// React Hot Reload does not support stateless function components as of now
+/* eslint-disable react/prefer-stateless-function */
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import styles from './style.scss';
+
+import Gallery from '../../components/gallery';
+
+import logo from '../../../img/logo.png';
+
+export default class Photos extends Component {
+    static propTypes = {
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = { opened: false, current: 0 };
+    }
+
+    render() {
+        return (
+            <div className={styles.fullHeight}>
+                <div className={styles.logoFrame}>
+                    <span className={styles.logoHelper}></span>
+                    <img src={logo} className={styles.logo} />
+                </div>
+                <span className={styles.galleryHelper} />
+                <div className={styles.gallery}>
+                    <Gallery />
+                </div>
+            </div>
+        );
+    }
+}
