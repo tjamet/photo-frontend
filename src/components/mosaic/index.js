7b1a1328c4b8a754521eada896dbdad5d32d5073
Thibault Jamet <tjamet@users.noreply.github.com>
2017-09-01 12:12:06 +0200

    Open lightbox when clicking on a mosaic image
    Signed-off-by: Thibault Jamet <tjamet@users.noreply.github.com>

--- src/components/mosaic/index.js
+++ src/components/mosaic/index.js
@@ -4,11 +4,23 @@ import styles from './style.scss';
 import Image from '../image';
 
 export default class Mosaic extends Component {
+
+    static propTypes = {
+        onLoad: PropTypes.func,
+        onError: PropTypes.func,
+        onClick: PropTypes.func,
+    }
+
     render() {
         return <div className={styles.mosaic}>
             {
-                this.props.images.map((elt) => (
-                    <Image key={elt.key} src={elt.thumbnail} />
+                this.props.images.map((elt, idx) => (
+                    <a href="#" key={elt.key}>
+                        <Image
+                            src={elt.thumbnail}
+                            onClick={(event) => { event.preventDefault(); if (this.props.onClick) this.props.onClick(idx) }}
+                        />
+                    </a>
                 ))
             }
         </div>
