815a4498b0a5a018006db05725a6df29029a2f56
Thibault Jamet <tjamet@users.noreply.github.com>
2017-09-01 12:13:05 +0200

    Use photos as default route
    Signed-off-by: Thibault Jamet <tjamet@users.noreply.github.com>

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from '../screens/app';
+import Photos from '../screens/photos';
 import Start from '../screens/start';
 import About from '../screens/about';
 
 export default () => (
   <Route component={ App }>
-    <IndexRoute component={ Start } />
+    <IndexRoute component={ Photos } />
     <Route component={ About } path="/about/" />
   </Route>
 );
