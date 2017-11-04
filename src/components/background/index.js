38c9f90add0f8f72dbd0423167cf0b7aca27c0c6
Thibault Jamet <tjamet@users.noreply.github.com>
2017-11-05 00:35:06 +0100

    Fix mixed content background URL
    Signed-off-by: Thibault Jamet <tjamet@users.noreply.github.com>

--- src/components/background/index.js
+++ src/components/background/index.js
@@ -7,7 +7,7 @@ import styles from './style.scss';
 export default class Background extends Component {
   render() {
     return (
-      <div className={styles.background} style={{backgroundImage: "url(http://thib-o.imgix.net/6D249D777C64390852FA9E69141A4D40?fit=max&fm=jpg&h=3000&q=95&w=3000&s=af618a288c01b0efee1eedc765d90773)"}}/>
+      <div className={styles.background} style={{backgroundImage: "url(//thib-o.imgix.net/6D249D777C64390852FA9E69141A4D40?fit=max&fm=jpg&h=3000&q=95&w=3000&s=af618a288c01b0efee1eedc765d90773)"}}/>
     );
   }
 }
